96621a9dab5a508756a61db6571fae75c8445039
mohamed <muridi51@gmail.com>
2021-08-09 10:23:41 +0100

    adding a title input, working

--- pastebin-front/src/components/InputPost.js
+++ pastebin-front/src/components/InputPost.js
@@ -2,10 +2,11 @@ import React, {Fragment, useState} from "react";
 
 const InputPost = () => {
     const [text, setText] = useState("")
+    const [title, setTitle]= useState("")
 
     const onSubmitForm = async e => {
         e.preventDefault();
-        const body = { text }
+        const body = { text, title }
         const response = await fetch("http://localhost:4000/newpost", {
             method: "POST",
             headers:{"Content-Type": "application/json"},
@@ -23,6 +24,12 @@ const InputPost = () => {
             className="form-control" 
             value={text} 
             onChange={e => setText (e.target.value)}
+            />
+            <input 
+            type="text" 
+            className="form-control" 
+            value={title} 
+            onChange={e => setTitle (e.target.value)}
             />
             <button className="btn btn-success">Submit</button>
         </form>
